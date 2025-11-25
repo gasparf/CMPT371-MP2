b2948b47673868a29571bead5bbf4de929d34aae
Ennil23541 <Cherrypcchan@gmail.com>
2025-11-24 19:37:25 -0800

    change sequence number handling, need to change how it handle data with different sequence number

--- WebServerUDP.py
+++ WebServerUDP.py
@@ -5,41 +5,42 @@ import time
 
 #initialize checksum and sequence number
 
-EXPECTED_SEQ = 0
+EXPECTED_SEQ = 1
+ack = "NAK"
+data = ""
+sequence_number = -1
 
 
-def handle_incoming_frame(frame, send_ack):
+def handle_incoming_frame(seq, data, send_ack):
     global EXPECTED_SEQ
 
-    seq = frame["sequence_number"] #seq is the number assigned by the client to this packet
-    data = frame["data"] #data is the payload
+    
 
     if seq == EXPECTED_SEQ: #correct sequence number, process packet
         print("Received new packet:", data) #frame is the raw incoming "packet" you received, already decoded to Python dict of "{"seq": 3, "data": "hello"}""
 
         # Process packet normally
-        process(data)
 
         # Send ACK
-        send_ack(seq)
+        msg = send_ack(seq) +  data.upper() 
 
         EXPECTED_SEQ += 1      # Next expected packet, not in a cycle
 
-    else:
-        # Duplicate or out-of-order packet – do NOT reprocess
+    else: # Duplicate or out-of-order packet – do NOT reprocess
         print("Duplicate packet, seq =", seq)
 
         # Still acknowledge, so sender knows it was received
-        send_ack(seq)
+        msg = send_ack(seq)
+    
+    return msg
 
 
 def send_ack(seq):
-    ack_frame = { "ack": seq }
-    raw_send(ack_frame)   # your low-level send
+    ack_message = str(seq)
+    ack_message += "ACK"
+    return ack_message   # your low-level send
 
 
-def process(data):
-    print("Processing:", data)
 
 
 # Define Server Port
@@ -57,9 +58,29 @@ while True: # Forever Loop
     # Read from UDP Socket into message & client address
     message, clientAddress = serverSocket.recvfrom(2048)
 
+    sentence = message.decode()
+    print(sentence)
+
+    lines = sentence.split("\r\n")
+    try: #load sequence number
+        if (len(lines) > 1):
+            sequence_number = lines[0]
+            ack = lines[2]
+    except:
+        print("Please input a number for sequence number")
+
+    try: #load data
+        if (len(lines) > 2):
+            for i in lines:
+                if(i > 1):
+                    data += lines[i]
+    except:
+        print("Please input a number for equence number")
+    
 
 
-    handle_incoming_frame(message, send_ack)
+    print(sequence_number, data)
+    modifiedMessage = handle_incoming_frame(sequence_number, data, send_ack)
 
 
 
